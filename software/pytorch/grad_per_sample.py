9653c4eafc0eb217381a21fca05eaf9e9cd0503b
Vektor Dewanto <vektor.dewanto@gmail.com>
2018-07-22 21:21:25 +1000

    fix

--- software/pytorch/grad_per_sample.py
+++ software/pytorch/grad_per_sample.py
@@ -2,6 +2,7 @@
 # https://discuss.pytorch.org/t/efficient-computation-of-per-sample-examples/18587/2
 
 import torch
+torch.manual_seed(12345)
 
 class LinearWithBatchGradFn(torch.autograd.Function):
     @staticmethod
@@ -14,28 +15,31 @@ class LinearWithBatchGradFn(torch.autograd.Function):
         grad_bias = grad_out if bias is not None else None
         return grad_out @ weight, (inp.unsqueeze(1)*grad_out.unsqueeze(2)), grad_bias
 
-linear_with_batch_grad = LinearWithBatchGradFn.apply
-
-
+# Init
 inp = torch.randn(5,2, requires_grad=True)
 weight = torch.randn(3,2, requires_grad=True)
 bias = torch.randn(3, requires_grad=True)
-
 gradw = torch.randn(5,3)
 
+# Grad is cumulative from all sampless
 a = torch.nn.functional.linear(inp, weight, bias)
 loss = (a*gradw).sum()
 
 gi, gw, gb = torch.autograd.grad(loss, [inp, weight, bias])
 
-a2 = linear_with_batch_grad(inp, weight, bias)
+# Approach: grad per sample
+a2 = LinearWithBatchGradFn.apply(inp, weight, bias)
 loss2 = (a2*gradw).sum()
+
 gi2, gw2, gb2 = torch.autograd.grad(loss2, [inp, weight, bias])
 
+# Compare
 print("grad weight", gw.shape, gw2.shape, torch.allclose(gw2.sum(0), gw))
-# print("grad bias", gb.shape, gb2.shape, torch.allclose(gb2.sum(0), gb))
-# print("grad inp stays the same for other layers networks", gi.shape, gi2.shape, torch.allclose(gi, gi2))
-
 print(gw.data)
 print(gw2.data)
 print(gw2.sum(0))
+
+print("grad bias", gb.shape, gb2.shape, torch.allclose(gb2.sum(0), gb))
+print("grad inp stays the same for other layers networks", gi.shape, gi2.shape, torch.allclose(gi, gi2))
+
+

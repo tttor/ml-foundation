d289bcc1ab013d36edd4b837b7e59ae22eb7fa5d
Vektor Dewanto <vektor.dewanto@gmail.com>
2018-07-26 18:12:13 +1000

    fix

--- software/pytorch/grad_per_sample.py
+++ software/pytorch/grad_per_sample.py
@@ -15,25 +15,25 @@ class LinearWithBatchGradFn(torch.autograd.Function):
         grad_bias = grad_out if bias is not None else None
         return grad_out @ weight, (inp.unsqueeze(1)*grad_out.unsqueeze(2)), grad_bias
 
-# Init
+# Init #########################################################################
 inp = torch.randn(5,2, requires_grad=True)
 weight = torch.randn(3,2, requires_grad=True)
 bias = torch.randn(3, requires_grad=True)
 gradw = torch.randn(5,3)
 
-# Approach: grad is cumulative from all sampless
+# Approach: grad is cumulative from all sampless ###############################
 a = torch.nn.functional.linear(inp, weight, bias)
 loss = (a*gradw).sum()
 
 gi, gw, gb = torch.autograd.grad(loss, [inp, weight, bias])
 
-# Approach: grad per sample
+# Approach: grad per sample ####################################################
 a2 = LinearWithBatchGradFn.apply(inp, weight, bias)
 loss2 = (a2*gradw).sum()
 
 gi2, gw2, gb2 = torch.autograd.grad(loss2, [inp, weight, bias])
 
-# Compare
+# Compare ######################################################################
 print("grad weight", gw.shape, gw2.shape, torch.allclose(gw2.sum(0), gw))
 print(gw.data)
 print(gw2.data)

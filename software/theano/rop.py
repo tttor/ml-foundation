b71c795bec42d09cb96c02a93419e4e286012fff
Vektor Dewanto <vektor.dewanto@gmail.com>
2018-10-02 20:09:41 +1000

    fix

--- software/theano/rop.py
+++ software/theano/rop.py
@@ -14,19 +14,26 @@ import theano.tensor as T
 # print(r)
 
 ################################################################################
-W = T.dmatrix('W'); W_val = [[1, 1], [1, 1]]
-x = T.dvector('x'); x_val = [0, 1]
-
+W = T.dmatrix('W')
+x = T.dvector('x')
 y = T.dot(x, W)
 fy = theano.function([W, x], y)
-ry = fy(W_val, x_val)
-print(ry)
 
-V = T.dmatrix('V'); V_val = [[2, 2], [2, 2]]
-JV = T.Rop(y, W, V)
-f = theano.function([W, x, V], JV)
-r = f(W_val, x_val, V_val)
-print(r)
+W_val = [[1, 1], [1, 1]]
+x_val = [0, 1]
+rfy = fy(W_val, x_val)
+print(rfy)
+
+# J, updates = theano.scan(lambda i, y, x : T.grad(y[i], x), sequences=T.arange(y.shape[0]), non_sequences=[y, x])
+# fj = theano.function([W, x], J, updates=updates)
+# rfj = fj([W_val, x_val])
+
+
+# V = T.dmatrix('V'); V_val = [[2, 2], [2, 2]]
+# JV = T.Rop(y, W, V)
+# f = theano.function([W, x, V], JV)
+# r = f(W_val, x_val, V_val)
+# print(r)
 
 # v = T.dvector('v')
 # Jv = T.Rop(y, W, v)
